501627cef5db8c4330f43991be77195ba649bbe5
Jessica Haynes <jkhaynes@iastate.edu>
2016-02-10 13:34:34 -0600

    Added simple program findLast and tests to homework 2

---
+++ Homework2/src/Tests/findLastTests.java
@@ -0,0 +1,18 @@
+package Tests;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Program.findLast;
+
+public class findLastTests {
+
+	@Test
+	public void test() {
+		findLast object = new findLast();
+		int[] x = {2, 3, 5};
+		int result = object.findLast(x, 2);
+		assertEquals(0, result);
+	}
+	
+}
